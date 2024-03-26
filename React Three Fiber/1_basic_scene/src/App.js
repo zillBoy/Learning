5d73109b08d4b942dff98883f528e8123d086ffc
Muhammad Usman <41048737+zillBoy@users.noreply.github.com>
2024-03-26 13:43:03 +0500

    [Learning/react-three-fiber] (#3)
    * [learning/react-three-fiber] adding initial files
    
    * [react-three-fiber/1-basic-scene] added a new component BasicScene (#2)

---
+++ React Three Fiber/1_basic_scene/src/App.js
@@ -0,0 +1,15 @@
+// React Dependencies
+import React from "react";
+
+// Internal Dependencies
+import BasicScene from "./components/1_basic_scene/BasicScene";
+
+const App = () => {
+  return (
+    <>
+      <BasicScene />
+    </>
+  );
+};
+
+export default App;
